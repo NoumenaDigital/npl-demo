5e4ae33c163c4291a4102d745dc88eade3f9efda
JeanHaiz <jean.haizmann@gmail.com>
2025-10-14 15:22:04 +0200

    greetee - update UI

--- webapp/src/components/code-preview.ts
+++ webapp/src/components/code-preview.ts
@@ -29,18 +29,18 @@ export class CodePreviewComponent extends HTMLElement {
         <div class="code-line"><span class="line-number">7</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;<span class="keyword">final</span> <span class="keyword">state</span> greeted;</span></div>
         <div class="code-line"><span class="line-number">8</span> <span class="line-code"></span></div>
         <div class="code-line"><span class="line-number">9</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;<span class="api-annotation">@api</span></span></div>
-        <div class="code-line"><span class="line-number">10</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;<span class="keyword">permission</span>[greeter] <span class="function">sayHello</span>() <span class="keyword">returns</span> Text | greeting {</span></div>
+        <div class="code-line"><span class="line-number">10</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;<span class="keyword">permission</span>[greeter] <span class="function">sayHello</span>(greetee: Text) <span class="keyword">returns</span> Text | greeting {</span></div>
         <div class="code-line"><span class="line-number">11</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="keyword">become</span> greeted;</span></div>
-        <div class="code-line"><span class="line-number">12</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="keyword">return</span> <span class="string">"Hello "</span> + <span class="function">getUsername</span>(greeter) + <span class="string">"!"</span>;</span></div>
+        <div class="code-line"><span class="line-number">12</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="keyword">return</span> <span class="string">"Hi "</span> + greetee + <span class="string">", "</span> + <span class="function">getUsername</span>(greeter) + <span class="string">" sends greetings!"</span>;</span></div>
         <div class="code-line"><span class="line-number">13</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;};</span></div>
         <div class="code-line"><span class="line-number">14</span> <span class="line-code">};</span></div>
         <div class="code-line"><span class="line-number">15</span> <span class="line-code"></span></div>
         <div class="code-line"><span class="line-number">16</span> <span class="line-code"><span class="keyword">function</span> <span class="function">getUsername</span>(party: Party) <span class="keyword">returns</span> Text -></span></div>
         <div class="code-line"><span class="line-number">17</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;party.<span class="function">claims</span>()</span></div>
-        <div class="code-line"><span class="line-number">18</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.<span class="function">getOrNone</span>(<span class="string">"preferred_username"</span>)</span></div>
-        <div class="code-line"><span class="line-number">19</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.<span class="function">getOrFail</span>()</span></div>
-        <div class="code-line"><span class="line-number">20</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.<span class="function">toList</span>()</span></div>
-        <div class="code-line"><span class="line-number">21</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.<span class="function">get</span>(<span class="number">0</span>);</span></div>
+        <div class="code-line"><span class="line-number">18</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;.<span class="function">getOrNone</span>(<span class="string">"preferred_username"</span>)</span></div>
+        <div class="code-line"><span class="line-number">19</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;.<span class="function">getOrFail</span>()</span></div>
+        <div class="code-line"><span class="line-number">20</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;.<span class="function">toList</span>()</span></div>
+        <div class="code-line"><span class="line-number">21</span> <span class="line-code">&nbsp;&nbsp;&nbsp;&nbsp;.<span class="function">get</span>(<span class="number">0</span>);</span></div>
         <div class="code-line"><span class="line-number">22</span> <span class="line-code"></span></div>
       </div>
     `;
